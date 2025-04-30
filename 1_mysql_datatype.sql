8ed6b83c0c8a1d7949b27979c8994249ea8f589a
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-04-30 08:22:41 +0600

    date

--- 1_mysql_datatype.sql
+++ 1_mysql_datatype.sql
@@ -6,4 +6,17 @@ DOUBLE ->
 DECIMAL -> 
  */
 
- 
+-- String DataType
+/* 
+CHAR -> CHAR(4) -> means 4 character
+VARCHAR -> 
+TEXT -> 
+ */
+
+ -- Date and Time DataType
+ /* 
+ DATE -> 
+ DATETIME -> 
+ TIMESTAMP -> 
+ YEAR -> 
+ */
